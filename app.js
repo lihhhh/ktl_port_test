4be67b02f892cb56edb04df5d04026d419ac66c9
lihao <lih@hzjs.co>
2017-08-11 14:26:42 +0800

    bug

--- app.js
+++ app.js
@@ -6,20 +6,19 @@ var cheerio = require('cheerio');
 var iconv = require('iconv-lite');
 
 
-function getHttp(option,callback){
-	http.get(option, function(res) {
-	    var chunks = [];
-	    res.on('data', function(chunk) {
-	        chunks.push(chunk);
-	    })
-	    res.on('end', function() {
-	    	var html = Buffer.concat(chunks).toString();
-	    	callback(html);
-	    })
-	    res.on('error',function(err){
-	    	console.log(err);
-	    })
-	})
+function getHttp(option, callback) {
+    http.get(option, function(res) {
+        var chunks = [];
+        res.on('data', function(chunk) {
+            chunks.push(chunk);
+        })
+        res.on('end', function() {
+            var html = Buffer.concat(chunks).toString();
+            callback(html);
+        })
+    }).on('error', (e) => {
+        console.error(e);
+    });
 }
 
 
@@ -27,93 +26,83 @@ app.get('/', function(req, res) {
     res.sendfile(__dirname + '/client/port_test.html');
 })
 
-app.get('/getData',function(req,res){
-	var option = {
-	    hostname: '192.168.10.133',
-	    port: '8080',
-	    path: '/ktl/help.jsp'
-	};
-	// fs.readFile(__dirname+'/getData.txt',function(err,text){
-	// 	if(err) return res.status(400).json({success:false,err:'错误'});
-	// 	var html = text;
-		
-	// })
-	getHttp(option,function(html){
-		var $ = cheerio.load(html);
+app.get('/getData', function(req, res) {
+    var option = {
+        hostname: '192.168.10.133',
+        port: '8080',
+        path: '/ktl/help.jsp'
+    };
+    getHttp(option, function(html) {
+        var $ = cheerio.load(html);
 
-		var result = [];
-		$('table').get().map(function(item){
-			var temp = {
-				title:'',
-				data:[]
-			};
-			temp.title = $(item).prev().text();
-			$(item).find('tr').get().map(function(tr){
-				temp.data.push({
-					name : $(tr).find('td').eq(1).text(),  //名称
-					url : $(tr).find('td a').eq(0).text(),		//url后缀
-					pointer_url : $(tr).find('td a').eq(0).attr('href') //目标详细页面 url地址
-				})
-			})
-			result.push(temp);
-		})
-		console.log(result);
-		res.status(200).json({success:true,result:result});
-	})
+        var result = [];
+        $('table').get().map(function(item) {
+            var temp = {
+                title: '',
+                data: []
+            };
+            temp.title = $(item).prev().text();
+            $(item).find('tr').get().map(function(tr) {
+                temp.data.push({
+                    name: $(tr).find('td').eq(1).text(), //名称
+                    url: $(tr).find('td a').eq(0).text(), //url后缀
+                    pointer_url: $(tr).find('td a').eq(0).attr('href') //目标详细页面 url地址
+                })
+            })
+            result.push(temp);
+        })
+        console.log(result);
+        res.status(200).json({ success: true, result: result });
+    })
 })
 
-app.get('/getDetail',function(req,res){
-	var query = req.query;
-	var option = {
-	    hostname: '192.168.10.133',
-	    port: '8080',
-	    path: '/ktl/'+query.pointer_url
-	};
+app.get('/getDetail', function(req, res) {
+    var query = req.query;
+    var option = {
+        hostname: '192.168.10.133',
+        port: '8080',
+        path: '/ktl/' + query.pointer_url
+    };
 
-	// fs.readFile(__dirname+'/detail.txt',function(err,text){
-	// 	if(err) return res.status(400).json({success:false,err:'错误'});
-		
-	// })
-	getHttp(option,function(html){
-		var $ = cheerio.load(html);
-		var result = [];
-		$('table').eq(0).find('tr').get().map(function(tr,index){
-			
-			if(index>0){
-				var temp = {};
-				$(tr).find('td').get().map(function(td,index){
-					if(index==1){
-						temp.field = $(td).text();
-					}
-				})
-				result.push(temp);
-			}
-			
-		})
-		console.log(result);
-		res.status(200).json({success:true,result:result});
-	})
+    getHttp(option, function(html) {
+        var $ = cheerio.load(html);
+        var result = [];
+        $('table').eq(0).find('tr').get().map(function(tr, index) {
+
+            if (index > 0) {
+                var temp = {
+                    argname:$(tr).find('td').eq(0).text(),
+                    field:$(tr).find('td').eq(1).text(),
+                    ms:$(tr).find('td').eq(2).text(),
+                };
+                result.push(temp);
+            }
+
+        })
+        console.log(result);
+        res.status(200).json({ success: true, result: result });
+    })
 })
 
-app.get('/getDetailData',function(req,res){
-	var query = req.query;
-	console.log(query);
-	var option = {
-	    hostname: '192.168.10.133',
-	    port: '8080',
-	    path: '/ktl/'+query.pointer_url+'?'
-	};
-	var url = [];
-	for(var key in query){
-		if(key!='pointer_url'){
-			url.push(key+'='+query[key]);
-		}
-	}
-	option.path+=url.join('&');
+app.get('/getDetailData', function(req, res) {
+    var query = req.query;
+    console.log(query);
+    var option = {
+        hostname: '192.168.10.133',
+        port: '8080',
+        path: '/ktl/' + query.pointer_url + '?'
+    };
+    var url = [];
+    for (var key in query) {
+        if (key != 'pointer_url') {
+            url.push(key + '=' + query[key]);
+        }
+    }
+    option.path += url.join('&');
 
-	getHttp(option,function(_d){
-		res.status(200).json({success:true,result:_d});
-	})
+    getHttp(option, function(_d) {
+        res.status(200).json({ success: true, result: _d });
+    })
 })
 
 app.listen(8888, function() {
